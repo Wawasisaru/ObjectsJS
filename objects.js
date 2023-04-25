670f3412125f5b32a1a8e9cd3ea4f8135acd4142
Wawasisaru <nsaruwawasi@gmil,.com>
2023-04-25 09:00:01 +0300

    first commit

---
+++ objects.js
@@ -0,0 +1,19 @@
+// given an array of objects, each object representing a person with a name and age property, write a function that returns a new array containing the names of all people who are 18 years old or older.
+// const people = [
+//   { name: 'Alice', age: 17 },
+//   { name: 'Eunice', age: 22 },
+//   { name: 'Charlie', age: 14 },
+//   { name: 'Max', age: 19 },
+// ];
+
+
+
+
+
+// Write a function that takes an array of objects, where each object represents a product with a name, price, and category property. The function should return an object that groups the products by their categories, with the category names as keys and the arrays of products as values.
+// const products = [
+//   { name: 'Laptop', price: 1200, category: 'Electronics' },
+//   { name: 'Shirt', price: 25, category: 'Clothing' },
+//   { name: 'Headphones', price: 80, category: 'Electronics' },
+//   { name: 'Shoes', price: 60, category: 'Clothing' },
+// ];
